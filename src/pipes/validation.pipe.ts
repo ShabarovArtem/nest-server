bc9f4b438f99f3246f16b8c82fed0a5b49384de8
ShabarovArtem <159617454+ShabarovArtem@users.noreply.github.com>
2025-02-17 14:37:52 +0300

    Update validation.pipe.ts

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,11 +1,8 @@
-//npm i class-validator class-transformer
 import {ArgumentMetadata, Injectable, PipeTransform} from "@nestjs/common";
 import {plainToClass} from "class-transformer";
 import {validate} from "class-validator";
 import {ValidationException} from "../exception/validation.exception";
 
-//pipe имеют 2 осн предназначения, 1 - преобраз входные данные, 2 - валидация основных данных (наш случай)
-//Валидация в данном контексте означает проверку подлинности и правильности JWT (JSON Web Token), содержащегося в заголовке авторизации запроса.
 @Injectable()
 export class ValidationPipe implements PipeTransform<any> {
     async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
