16a762772b1220cd503cd24f90c02b7929888602
ShabarovArtem <159617454+ShabarovArtem@users.noreply.github.com>
2025-02-17 14:37:31 +0300

    Update roles.guard.ts

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -12,14 +12,12 @@ import {JwtService} from "@nestjs/jwt";
 import {Reflector} from "@nestjs/core";
 import {ROLES_KEY} from "./roles-auth.decorator";
 
-//создаём для ограничения, например не зарегестрированным пользователям
 @Injectable()
 export class RolesGuard implements CanActivate {
 
     constructor(private jwtService: JwtService,
                 private reflector: Reflector) {}
 
-//Метод canActivate является основным методом, который определяет, может ли запрос пройти.
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         try {
             const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
@@ -30,20 +28,14 @@ export class RolesGuard implements CanActivate {
                 return true;
             }
             const req = context.switchToHttp().getRequest()
-            //Здесь мы извлекаем заголовок авторизации из заголовков запроса. Этот заголовок должен содержать токен JWT.
             const authHeader = req.headers.authorization;
-            //Мы разделяем заголовок авторизации на две части: тип токена (bearer) и сам токен (token).
             const bearer = authHeader.split(' ')[0]
             const token = authHeader.split(' ')[1]
-            //Проверка типа токена и наличия токена
             if (bearer !== 'Bearer' || !token){
                 throw new UnauthorizedException({message: 'Пользователь не авторизован'})
             }
-//Мы используем jwtService для проверки токена. Если токен действителен, jwtService.verify(token) возвращает объект пользователя, закодированный в токене.
             const user = this.jwtService.verify(token);
-            //Если токен успешно проверен, мы присваиваем объект пользователя в запрос. Это позволяет использовать информацию о пользователе в других частях приложения.
             req.user = user;
-            //проверяем есть ли необходимая роль
             return user.roles.some(role => requiredRoles.includes(role.value));
 
         } catch (e) {
