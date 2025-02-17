b463fd3b9640dd0db5cab9ebff73029515b251dd
ShabarovArtem <159617454+ShabarovArtem@users.noreply.github.com>
2025-02-17 14:40:17 +0300

    Update user-roles.model.ts

--- src/roles/user-roles.model.ts
+++ src/roles/user-roles.model.ts
@@ -3,15 +3,12 @@ import {ApiProperty} from "@nestjs/swagger";
 import {User} from "../users/users.model";
 import {Role} from "./roles.model";
 
-//создали третью таблицу, из-за связи много-ко многим
-//createdAt - дата создания, updatedAt - дата обновления, тут они не нужны
 @Table({tableName: 'user_roles', createdAt: false, updatedAt: false})
 export class UserRoles extends Model<UserRoles> {
 
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
-    //помечаем что внешний ключ
     @ForeignKey(() => Role)
     @Column({type: DataType.INTEGER})
     roleId: number;
