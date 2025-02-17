16850c4ee6cb4f6e1689edbc92ed84b824b84df8
ShabarovArtem <159617454+ShabarovArtem@users.noreply.github.com>
2025-02-17 14:39:43 +0300

    Update roles.model.ts

--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,20 +1,15 @@
-//опишем схему того, как пользователь будет сохраняться в базе данных
 import {BelongsToMany, Column, DataType, Model, Table} from 'sequelize-typescript';
 import {ApiProperty} from "@nestjs/swagger";
 import {User} from "../users/users.model";
 import {UserRoles} from "./user-roles.model";
 
-//необходим для создания объекта класса User
 interface RoleCreationAttrs {
     value: string;
     description: string;
 }
 
-//Чтобы класс стал таблицей помечаем его @Table, tableName: её название
 @Table({tableName: 'roles'})
 export class Role extends Model<Role, RoleCreationAttrs> {
-    //чтобы были колонки @Column
-    //type - тип данных, unique - уникальность, autoIncrement и так понятно, primaryKey - первичный ключ
     @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
@@ -27,7 +22,6 @@ export class Role extends Model<Role, RoleCreationAttrs> {
     @Column({type: DataType.STRING, allowNull: false})
     description: string;
 
-    //(связь многие ко многим) это связанно с файлом users.model
     @BelongsToMany(() => User, (role) => UserRoles)
     users: User[];
 
