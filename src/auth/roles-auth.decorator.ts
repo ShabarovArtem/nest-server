405ca8d1ee046b489c8176159f2695919f83ce1b
ShabarovArtem <159617454+ShabarovArtem@users.noreply.github.com>
2025-02-17 14:37:09 +0300

    Update roles-auth.decorator.ts

--- src/auth/roles-auth.decorator.ts
+++ src/auth/roles-auth.decorator.ts
@@ -1,6 +1,4 @@
-//создадим конст с ключом, и по этому ключу сможем получать метаданные внутри guard
 import {SetMetadata} from "@nestjs/common";
 
 export const ROLES_KEY = 'roles';
-//создадим Roles которая декоратор, принимает массив стринговых ролей
 export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
